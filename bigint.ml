dc30ff9ce09cc0db08090526dbede01067433685
William Cork <willfcork@gmail.com>
2014-11-29 15:18:42 -0800

    Added comments and modified output to match that of dc.

--- bigint.ml
+++ bigint.ml
@@ -28,10 +28,12 @@ module Bigint = struct
     let strsub    = String.sub
     let zero      = Bigint (Pos, [])
 
+    (*Used for debugging. Prints a whole list, space delimited*)
     let rec print_list = function 
         [] -> ()
         | e::l -> print_int e ; print_string " " ; print_list l
 
+    (*Removes al leading zeros from the list.*)
     let trimzeros list =
         let rec trimzeros' list' = match list' with
             | []       -> []
@@ -39,10 +41,11 @@ module Bigint = struct
             | car::cdr ->
                  let cdr' = trimzeros' cdr
                  in  match car, cdr' with
-                     | 0, [] -> []
-                     | car, cdr' -> car::cdr'
+                    | 0, [] -> []
+                    | car, cdr' -> car::cdr'
         in trimzeros' list
 
+    (*Given.*)
     let charlist_of_string str = 
         let last = strlen str - 1
         in  let rec charlist pos result =
@@ -51,6 +54,7 @@ module Bigint = struct
             else charlist (pos - 1) (str.[pos] :: result)
         in  charlist last []
 
+    (*Given. Converts string to bigint*)
     let bigint_of_string str =
         let len = strlen str
         in  let to_intlist first =
@@ -60,17 +64,19 @@ module Bigint = struct
             in  if   len = 0
                 then zero
                 else if   str.[0] = '_'
-                     then Bigint (Neg, trimzeros (to_intlist 1))
-                     else Bigint (Pos, trimzeros (to_intlist 0))
+                    then Bigint (Neg, trimzeros (to_intlist 1))
+                    else Bigint (Pos, trimzeros (to_intlist 0))
 
+    (*Given. Converts bigint to string*)
     let string_of_bigint (Bigint (sign, value)) =
         match value with
         | []    -> "0"
         | value -> let reversed = reverse value
                    in  strcat ""
-                       ((if sign = Pos then "" else "-") ::
+                      ((if sign = Pos then "" else "-") ::
                         (map string_of_int reversed))
 
+    (*cmp helper*)
     let rec cmp' list1 list2 = match (list1, list2) with
         | [], []                 ->  0
         | list1, []              ->  1
@@ -83,24 +89,27 @@ module Bigint = struct
                     else (if car1 < car2
                     then -1
                     else 0))
-               else retval
+              else retval
 
+    (*Works like strcmp from c but with bigints.*)
     let cmp (Bigint (neg1, value1)) (Bigint (neg2, value2)) =
         if neg1 = neg2
         then cmp' value1 value2
         else if neg1 = Neg
-             then -1
-             else 1
+            then -1
+            else 1
 
+    (*add helper*)
     let rec add' list1 list2 carry = match (list1, list2, carry) with
-        | list1, [], 0       -> list1
-        | [], list2, 0       -> list2
-        | list1, [], carry   -> add' list1 [carry] 0
-        | [], list2, carry   -> add' [carry] list2 0
+        | list1, [], 0                  -> list1
+        | [], list2, 0                  -> list2
+        | list1, [], carry              -> add' list1 [carry] 0
+        | [], list2, carry              -> add' [carry] list2 0
         | car1::cdr1, car2::cdr2, carry ->
           let sum = car1 + car2 + carry
           in  sum mod radix :: add' cdr1 cdr2 (sum / radix)
 
+    (*sub helper*)
     let rec sub' list1 list2 steal = match (list1, list2, steal) with
         | list1, [], 0           -> list1
         | [], list2, 0           -> failwith "sub: list2 > list1"
@@ -108,19 +117,21 @@ module Bigint = struct
           if car1 = 0
           then 9 :: (sub' cdr1 [] 1)
           else let dif = car1 - steal*1
-               in dif :: (sub' cdr1 [] 0)
-        | [], list2, steal       -> failwith "sub: WTF"
+              in dif :: (sub' cdr1 [] 0)
+        | [], list2, steal              -> failwith "sub: WTF"
         | car1::cdr1, car2::cdr2, steal ->
           
           if car2 > (car1 - steal*1)
           then let dif = ((car1 + 10) - steal*1) - car2
-               in dif :: (sub' cdr1 cdr2 1)
+              in dif :: (sub' cdr1 cdr2 1)
           else let dif = (car1 - steal*1) - car2
-               in dif :: (sub' cdr1 cdr2 0)
+              in dif :: (sub' cdr1 cdr2 0)
 
+    (*doubles a bigint*)
     let double_bigint_list number =
         add' number number 0
 
+    (*mul helper*)
     let rec mul' (multiplier, powerof2, multiplicand') =
         if (cmp' powerof2 multiplier) = 1
         then multiplier, []
@@ -130,8 +141,9 @@ module Bigint = struct
          in if (cmp' powerof2 remainder) = 1
             then remainder, product
             else (trimzeros(sub' remainder powerof2 0)),
-                 (add' product multiplicand' 0)
+                (add' product multiplicand' 0)
 
+    (*adds two bigints together. Handles signage*)
     let add (Bigint (neg1, value1)) (Bigint (neg2, value2)) =
         if neg1 = neg2
         then Bigint (neg1, add' value1 value2 0)
@@ -140,16 +152,18 @@ module Bigint = struct
             then Bigint (neg1, trimzeros(sub' value1 value2 0))
             else Bigint (neg2, trimzeros(sub' value2 value1 0))
 
+    (*Subtracts a bigint from a bigint*)
     let sub (Bigint (neg1, value1)) (Bigint (neg2, value2)) =
         if neg1 = neg2
         then (if (cmp' value2 value1) = 1
-             then Bigint (Neg, trimzeros(sub' value2 value1 0))
-             else Bigint (Pos, trimzeros(sub' value1 value2 0)))
+            then Bigint (Neg, trimzeros(sub' value2 value1 0))
+            else Bigint (Pos, trimzeros(sub' value1 value2 0)))
         else 
             (if (cmp' value1 value2) = 1
             then Bigint (neg2, add' value1 value2 0)
             else Bigint (neg1, add' value2 value1 0))
 
+    (*Multiplies two bigints together.*)
     let mul (Bigint (neg1, value1)) (Bigint (neg2, value2)) =
         let _, product =
             mul' (value1, [1], value2) in
@@ -157,6 +171,7 @@ module Bigint = struct
                 then Bigint (Pos, product)
                 else Bigint (Neg, product)
 
+    (*divrem helper*)
     let rec divrem' (dividend, powerof2, divisor') =
         if (cmp' divisor' dividend) = 1
         then [0], dividend
@@ -164,31 +179,36 @@ module Bigint = struct
                  divrem' (dividend, double_bigint_list powerof2,
                                     double_bigint_list divisor')
              in  if (cmp' divisor' remainder) = 1
-                 then quotient, remainder
-                 else (add' quotient powerof2 0),
+                then quotient, remainder
+                else (add' quotient powerof2 0),
                       (trimzeros(sub' remainder divisor' 0))
 
+    (*Returns a tuple of quotient and remainder*)
     let divrem ((Bigint (neg1, value1)), (Bigint (neg2, value2))) =
         let quotient, remainder = divrem' (value1, [1], value2)
         in if neg1 = neg2
-           then Bigint (Pos, quotient),Bigint (Pos, remainder)
-           else Bigint (Neg, quotient),Bigint (Pos, remainder)
+          then Bigint (Pos, quotient),Bigint (Pos, remainder)
+          else Bigint (Neg, quotient),Bigint (Pos, remainder)
 
+    (*Returns division of bigints.*)
     let div (Bigint (neg1, value1)) (Bigint (neg2, value2)) =
         let quotient, _ = divrem ((Bigint (neg1, value1)),
-                                 (Bigint (neg2, value2)))
+                                (Bigint (neg2, value2)))
         in quotient
 
+    (*Returns remainder from division of bigints*)
     let rem (Bigint (neg1, value1)) (Bigint (neg2, value2)) =
         let _, remainder = divrem ((Bigint (neg1, value1)),
                                   (Bigint (neg2, value2)))
         in remainder
 
+    (*Returns boolean if the bigint is even*)
     let even (Bigint (neg, number)) =
         let _, remainder = rem (Bigint (neg, number)),
-                               (Bigint (Pos, [2]))
+                              (Bigint (Pos, [2]))
             in (cmp remainder zero) = 0
 
+    (*pow helper*)
     let rec pow' ((Bigint (neg1, base)), (Bigint (neg2, expt)),
         (Bigint (neg3, result))) =
         match (Bigint (neg2, expt)) with
@@ -197,13 +217,14 @@ module Bigint = struct
             (Bigint (neg3, result))
         | (Bigint (neg2, expt)) when even (Bigint (neg2, expt)) ->
             pow' (mul (Bigint (neg1, base)) (Bigint (neg1, base)),
-                 (div (Bigint (neg2, expt)) (Bigint (Pos, [2]))),
-                 (Bigint (neg3, result)))
+                (div (Bigint (neg2, expt)) (Bigint (Pos, [2]))),
+                (Bigint (neg3, result)))
         | (Bigint (neg2, expt))                                 ->
             pow' ((Bigint (neg1, base)),
-                 (sub (Bigint (neg2, expt)) (Bigint (Pos, [1]))),
-                 (mul (Bigint (neg1, base)) (Bigint (neg3, result))))
+                (sub (Bigint (neg2, expt)) (Bigint (Pos, [1]))),
+                (mul (Bigint (neg1, base)) (Bigint (neg3, result))))
 
+    (*raises the first bigint to the power of the second*)
     let pow (Bigint (neg1, value1)) (Bigint (neg2, value2)) =
         if (cmp (Bigint (neg2, value2)) zero) = -1
             then pow' 
--- maindc.ml
+++ maindc.ml
@@ -29,91 +29,90 @@ let explode s =
   exp (String.length s - 1) []
 
 let print_number number = 
-    let bignumstring = explode (string_of_bigint number)
-    in let rec print_number' number counter =
-       match (number, counter) with
-       | [],_          -> ()
-       | c::d,69       -> 
-            printf "\\\n%c" c;
-            print_number' d 1
-       | c::d,_        ->
-            printf "%c" c;
-            print_number' d (counter+1)
-    in print_number' bignumstring 0;
-       printf "\n%!"
+  let bignumstring = explode (string_of_bigint number)
+  in let rec print_number' number counter =
+    match (number, counter) with
+      | [],_          -> ()
+      | c::d,69       -> 
+        printf "\\\n%c" c;
+        print_number' d 1
+      | c::d,_        ->
+        printf "%c" c;
+        print_number' d (counter+1)
+      in print_number' bignumstring 0;
+        printf "\n%!"
 
-let print_stackempty () = eprintf "stack empty\n%!"
+let print_stackempty () = eprintf "dc: stack empty\n%!"
 
 let executereg (thestack: stack_t) (oper: char) (reg: int) =
-    try match oper with
-        | 'l' -> 
-            if (Hashtbl.mem registers reg)
-            then push (Hashtbl.find registers reg) thestack
-            else printf "register '%c' (0%o) is empty\n%!"
-                                            (char_of_int reg) reg
-        | 's' ->
-            Hashtbl.replace registers reg (pop thestack)
-        | _   -> printf "0%o 0%o is unimplemented\n%!" (ord oper) reg
-    with Stack.Empty -> print_stackempty()
+  try match oper with
+    | 'l' -> 
+      if (Hashtbl.mem registers reg)
+      then push (Hashtbl.find registers reg) thestack
+      else printf "dc: register '%c' (0%o) is empty\n%!"
+        (char_of_int reg) reg
+    | 's' ->
+      Hashtbl.replace registers reg (pop thestack)
+    | _   -> printf "0%o 0%o is unimplemented\n%!" (ord oper) reg
+      with Stack.Empty -> print_stackempty()
 
 let executebinop (thestack: stack_t) (oper: binop_t) (operc: char) =
-    try let right = pop thestack
-        in  try let left = pop thestack
-                in  if (operc = '/')
-                       && ((Bigint.cmp right Bigint.zero) = 0)
-                    then (eprintf "divide by zero\n%!";
-                         push left thestack;
-                         push right thestack)
-                    else push (oper left right) thestack
-            with Stack.Empty -> (print_stackempty ();
-                                push right thestack)
+  try let right = pop thestack
+    in  try let left = pop thestack
+      in  if (operc = '/')
+        && ((Bigint.cmp right Bigint.zero) = 0)
+          then (eprintf "dc: divide by zero\n%!";
+            push left thestack;
+            push right thestack)
+          else push (oper left right) thestack
+        with Stack.Empty -> (print_stackempty ();
+          push right thestack)
     with Stack.Empty -> print_stackempty ()
 
 let execute (thestack: stack_t) (oper: char) =
-    try match oper with
-        | '+'  -> executebinop thestack Bigint.add oper
-        | '-'  -> executebinop thestack Bigint.sub oper
-        | '*'  -> executebinop thestack Bigint.mul oper
-        | '/'  -> executebinop thestack Bigint.div oper
-        | '%'  -> executebinop thestack Bigint.rem oper
-        | '^'  -> executebinop thestack Bigint.pow oper
-        | 'c'  -> Stack.clear thestack
-        | 'd'  -> push (Stack.top thestack) thestack
-        | 'f'  -> Stack.iter print_number thestack
-        | 'l'  -> failwith "operator l scanned with no register"
-        | 'p'  -> print_number (Stack.top thestack)
-        | 's'  -> failwith "operator s scanned with no register"
-        | '\n' -> ()
-        | ' '  -> ()
-        | _    -> printf "0%o is unimplemented\n%!" (ord oper)
-    with Stack.Empty -> print_stackempty()
+  try match oper with
+    | '+'  -> executebinop thestack Bigint.add oper
+    | '-'  -> executebinop thestack Bigint.sub oper
+    | '*'  -> executebinop thestack Bigint.mul oper
+    | '/'  -> executebinop thestack Bigint.div oper
+    | '%'  -> executebinop thestack Bigint.rem oper
+    | '^'  -> executebinop thestack Bigint.pow oper
+    | 'c'  -> Stack.clear thestack
+    | 'd'  -> push (Stack.top thestack) thestack
+    | 'f'  -> Stack.iter print_number thestack
+    | 'l'  -> failwith "dc: operator l scanned with no register"
+    | 'p'  -> print_number (Stack.top thestack)
+    | 's'  -> failwith "dc: operator s scanned with no register"
+    | '\n' -> ()
+    | ' '  -> ()
+    | _    -> printf "dc: 0%o is unimplemented\n%!" (ord oper)
+  with Stack.Empty -> print_stackempty()
 
 let toploop (thestack: stack_t) inputchannel =
-    let scanbuf = Lexing.from_channel inputchannel in
-    let rec toploop () = 
-        try  let nexttoken = Scanner.scanner scanbuf
-             in  (match nexttoken with
-                 | Number number       -> push number thestack
-                 | Regoper (oper, reg) -> executereg thestack oper reg
-                 | Operator oper       -> execute thestack oper
-                 );
-             toploop ()
-        with End_of_file -> printf "%!";(*printf "End_of_file\n%!";*)
-    in  toploop ()
+  let scanbuf = Lexing.from_channel inputchannel in
+  let rec toploop () = 
+    try  let nexttoken = Scanner.scanner scanbuf
+      in  (match nexttoken with
+        | Number number       -> push number thestack
+        | Regoper (oper, reg) -> executereg thestack oper reg
+        | Operator oper       -> execute thestack oper
+        );
+      toploop ()
+    with End_of_file -> printf "%!";
+  in  toploop ()
 
 let readfiles () =
-    let thestack : bigint Stack.t = Stack.create ()
-    in  ((if Array.length Sys.argv > 1 
-         then try  let thefile = open_in Sys.argv.(1)
-                   in  toploop thestack thefile
-              with Sys_error message -> (
-                   printf "%s: %s\n%!" Sys.argv.(0) message;
-                   exit 1));
-        toploop thestack stdin)
+  let thestack : bigint Stack.t = Stack.create ()
+  in  ((if Array.length Sys.argv > 1 
+    then try  let thefile = open_in Sys.argv.(1)
+      in  toploop thestack thefile
+        with Sys_error message -> (
+          printf "%s: %s\n%!" Sys.argv.(0) message;
+          exit 1));
+    toploop thestack stdin)
 
 let interact () =
-    let thestack : bigint Stack.t = Stack.create ()
-    in  toploop thestack stdin
+  let thestack : bigint Stack.t = Stack.create ()
+  in  toploop thestack stdin
 
 let _ = if not !Sys.interactive then readfiles ()
-
